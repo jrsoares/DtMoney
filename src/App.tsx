ab3e02c9d500b69ad286ec96bd23eb534380904f
Jr.Soares <juniorsoaresti@gmail.com>
2023-04-18 10:54:31 -0400

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import {ThemeProvider} from "styled-components";
 import {defaultTheme} from "./styles/themes/default";
 import {GlobalStyle} from "./styles/global";
+import {Transactions} from "./Pages/Transactions";
 
 export function App() {
   return (
 <ThemeProvider theme={defaultTheme}>
     <GlobalStyle/>
-    <h1>Dt Money</h1>
+   <Transactions/>
 </ThemeProvider>
   )
 }
